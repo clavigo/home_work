0ea327483bcd2e1a56727b3b694cd2b1aaf2db20
clavigo <71872695+clavigo@users.noreply.github.com>
2020-10-15 17:22:22 +0300

    lab 1(3)

---
+++ lab 1(3)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, m;
+
+            Console.WriteLine("Введіть число n: ");
+            n = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть число m: ");
+            m = double.Parse(Console.ReadLine());
+
+            if (n>1 & n<5 & m>1 & m<5 )
+            {
+      
+
+                Console.WriteLine(Math.Pow(m, n));
+            }
+            else
+            {
+                Console.WriteLine("Числа повинні бути від 1 до 5.");
+            }
+            
+            
+        }
+    }
+}
