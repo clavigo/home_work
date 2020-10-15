395832df3bd7a651868eee7ad4e02aed98297db9
clavigo <71872695+clavigo@users.noreply.github.com>
2020-10-15 17:21:05 +0300

    lab 1(1)

---
+++ lab 1(1)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть радіус кола: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * (radius* radius);
+            Console.WriteLine("Площа: "+area);
+        }
+    }
+}
