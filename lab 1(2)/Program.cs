6e20ef64c1c789da85277f1f732a5131c003aa71
clavigo <71872695+clavigo@users.noreply.github.com>
2020-10-15 17:21:50 +0300

    lab 1(2)

---
+++ lab 1(2)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть сторону куба: ");
+            double side = Convert.ToDouble(Console.ReadLine());
+            double volume = side * side * side;
+            Console.WriteLine("Об`єм: " + volume);
+        }
+    }
+}
