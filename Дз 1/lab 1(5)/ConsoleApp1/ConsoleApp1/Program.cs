280413377d239cf2fa3f6d204e69035117d38ff0
clavigo <71872695+clavigo@users.noreply.github.com>
2020-11-12 11:05:01 +0200

    дз

---
+++ Дз 1/lab 1(5)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float input;
+            Console.Write("Введіть число: ");
+            input = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Integral part: {Math.Truncate(input)}. Rounded: {Math.Round(input)}.");     
+        }
+    }
+}
---
+++ Дз 1/lab 1(6)/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b;
+            Console.Write("Ведіть число: ");
+            a = float.Parse(Console.ReadLine());
+            b = (float)Math.Sqrt(Math.Ceiling(a));
+            Console.WriteLine($"{b}");
+        }
+    }
+}
